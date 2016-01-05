6c57b409fdc0c3a209907e11f0f7b77525a9d5b6
Vitrum <vitrum.cn@gmail.com>
2016-01-05 15:36:15 +0800

    update shell for mac

--- to_gh.sh
+++ to_gh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git checkout master
 gitbook build .
-cp -rf _book/* ~/tmp/gitbook
+cp -R _book/* ~/tmp/gitbook
 git checkout gh-pages
-cp -rf ~/tmp/gitbook .
+cp -R ~/tmp/gitbook .
 git add .
 git commit -m 'Publish by shell'
 git push origin gh-pages
