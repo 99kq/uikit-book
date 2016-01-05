3ce6254d46d4502813f88ad7c6e747928b266d27
Vitrum <vitrum.cn@gmail.com>
2016-01-05 15:42:14 +0800

    Publish by shell

--- to_gh.sh
+++ to_gh.sh
@@ -3,7 +3,7 @@ git checkout master
 gitbook build .
 cp -R _book/* ~/tmp/gitbook
 git checkout gh-pages
-cp -R ~/tmp/gitbook .
+cp -R ~/tmp/gitbook/* .
 git add .
 git commit -m 'Publish by shell'
 git push origin gh-pages
